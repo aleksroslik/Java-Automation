d26dfc9c1743140cdf30dbd020e4197709f3fe13
JavaClass <aleksroslik@gmail.com>
2021-01-01 16:37:53 +0100

    ramki - cwiczenie

---
+++ src/test/java/Ramki.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Ramki
+{
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1295, 730));
+        driver.manage().window().setPosition(new Point(10,40));
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.navigate().to("https://www.nasa.gov/topics/history/index.html");
+
+        wait = new WebDriverWait(driver, 5);
+
+    }
+
+    @AfterEach
+    public void Close()
+    {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void frameExamples()
+    {
+        WebElement iFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@id='twitter-widget-0']")));
+        //przelaczanie sie w kontekst ramki
+        driver.switchTo().frame(iFrame);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'View on Twitter')]")));
+        //przelaczanie sie z powrotem na glowna stronke
+        driver.switchTo().defaultContent();
+        driver.findElement(By.cssSelector(".navbar-header>.logo")).click();
+    }
+}
